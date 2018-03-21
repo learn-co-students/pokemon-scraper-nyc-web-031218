dd4049e8cb0b958d38e72ea3b9ac757ce392185b
Helen Liutongco <liutongcoh@gmail.com>
2018-03-21 12:54:41 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,51 @@
+require 'pry'
+
 class Pokemon
+  attr_accessor :name, :type, :hp, :id, :db
+  # attr_reader
+  def initialize(hash, hp = 60)
+    @id = hash[:id]
+    @name = hash[:name]
+    @type = hash[:type]
+    @db = hash[:db]
+    @hp = hp
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (?, ?);
+    SQL
+
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT * FROM pokemon WHERE id = ? LIMIT 1;
+    SQL
+    arr = db.execute(sql, id).flatten
+
+    new_pokemon = {}
+    new_pokemon[:id] = arr[0]
+    new_pokemon[:name] = arr[1]
+    new_pokemon[:type] = arr[2]
+    new_pokemon[:hp] = arr[3]
+
+    self.new(new_pokemon)
+  end
+
+  def alter_hp(hp, db)
+    sql = <<-SQL
+      UPDATE pokemon SET hp = ? WHERE id = ?;
+    SQL
+
+    hp_sql = <<-SQL
+      SELECT hp FROM pokemon WHERE id = ? LIMIT 1;
+    SQL
+
+    db.execute(sql, hp, self.id)
+    testthis = db.execute(hp_sql, self.id).flatten.first
+    self.hp = testthis
+    testthis
+  end
 end
